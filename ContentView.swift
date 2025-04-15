1691b493667c05bee9ae796555faa52fa4335982
Christopher Sebastian <csebastian8396@stu.d214.org>
2025-04-15 11:28:50 -0700

    I deleted some unessecary code.

--- ContentView.swift
+++ ContentView.swift
@@ -4,9 +4,6 @@ struct ContentView: View {
     @State var salary1: Int = 0
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
             Text("Hello, world!")
         }
     }
