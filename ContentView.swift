a6b449efe11119f17d6c0e6d7ecc6f5d106db5e2
Jayden Kim <jkim8208@stu.d214.org>
2025-04-15 13:30:11 -0500

    I changed the salary1 variable to an optional. I added a textfield and made it centered.

--- ContentView.swift
+++ ContentView.swift
@@ -1,13 +1,15 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var salary1: Int = 0
+    @State var salary1: Int!
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
+            TextField("Enter your salary!", value: $salary1, format: .number)
+                .multilineTextAlignment(.center)
         }
     }
 }
