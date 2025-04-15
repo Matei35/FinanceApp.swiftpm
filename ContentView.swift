0c8c13aa33f7c3c5411a80738ef8b7a278fccbf1
Matei Donisa <mdonisa8105@stu.d214.org>
2025-04-15 13:38:51 -0500

    I created the variable tax

--- ContentView.swift
+++ ContentView.swift
@@ -2,15 +2,10 @@ import SwiftUI
 
 struct ContentView: View {
     @State var salary1: Int!
+    @State var Tax: Int!
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
             TextField("Enter your salary!", value: $salary1, format: .number)
                 .multilineTextAlignment(.center)
-            
-        }
+        
     }
 }
