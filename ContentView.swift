c2deca629e110a7dff37c7ff5bd6aa6b9b5a5fa2
Matei Donisa <mdonisa8105@stu.d214.org>
2025-04-22 12:39:52 -0500

    I created a tittle and some modifications

--- ContentView.swift
+++ ContentView.swift
@@ -6,7 +6,10 @@ struct ContentView: View {
     @State var salary1: Int!
     @State var Tax: Int!
     var body: some View {
-            TextField("Enter your salary!", value: $salary1, format: .number)
+            Text("J.M.C. Finance App")
+            .font(.largeTitle)
+            .foregroundStyle(.green)
+        TextField("Enter your salary!", value: $salary1, format: .number)
                 .multilineTextAlignment(.center)
         
     }
