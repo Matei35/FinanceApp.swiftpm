59e675c4f48876f6bb7a2a3357b397e3139134bd
Christopher Sebastian <csebastian8396@stu.d214.org>
2025-04-24 11:04:00 -0700

    I shortened the details of a TextField.

--- RetirementAccount.swift
+++ RetirementAccount.swift
@@ -3,7 +3,8 @@ import SwiftUI
 struct RetirementAccountView: View {
     @State var retirementAccount: Int = 0
         var body: some View{
-            TextField("Enter the ammount of money invested into your 401k", value: $retirementAccount, format: .number)
+            
+            TextField("Money for 401k", value: $retirementAccount, format: .number)
                 .multilineTextAlignment(.center)
     }
 }
