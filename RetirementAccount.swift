d8ead62451d4b5c9ba4f853ed57bc3ab070fa434
Christopher Sebastian <csebastian8396@stu.d214.org>
2025-04-28 11:35:31 -0700

    I Added the Age limit.

--- RetirementAccount.swift
+++ RetirementAccount.swift
@@ -4,17 +4,60 @@ struct RetirementAccountView: View {
     @State var retirementAccount: Double = 0
     @State var takeHomePay: Double = 0
     @State var moneyPost401k: Double =  0
-    @State var age: Int = 0
+    @State var age: Double = 0
+    @State var output: String = ""
         var body: some View{
             Text("Enter your age")
-            Text("Enter your salary post tax")
-            TextField("Enter your salary post-tax!", value: $takeHomePay, format: .number)
+            TextField("", value: $age, format: .number)
+                .multilineTextAlignment(.center)
+            Button("Find out Maximum Limit for 401k"){
+                if age <= 49 {
+                    output = "The maximum amount you can put in your 401k is $23,500"
+                }
+                if age == 50 {
+                    output = "The maximum amount you can put in your 401k is $31,000"
+                }
+                if age == 51 {
+                    output = "The maximum amount you can put in your 401k is $31,000"
+                }
+                if age == 52 {
+                    output = "The maximum amount you can put in your 401k is $31,000"
+                }
+                if age == 53 {
+                    output = "The maximum amount you can put in your 401k is $31,000"
+                }
+                if age == 54 {
+                    output = "The maximum amount you can put in your 401k is $31,000"
+                }
+                if age == 55 {
+                    output = "The maximum amount you can put in your 401k is $31,000"
+                }
+                if age == 56 {
+                    output = "The maximum amount you can put in your 401k is $31,000"
+                }
+                if age == 57 {
+                    output = "The maximum amount you can put in your 401k is $31,000"
+                }
+                if age == 58 {
+                    output = "The maximum amount you can put in your 401k is $31,000"
+                }
+                if age == 59 {
+                    output = "The maximum amount you can put in your 401k is $31,000"
+                }
+                if age >= 60 {
+                    output = "The maximum amount you can put in your 401k is $34,750"
+                }
+            }
+            Text("\(output)")
+            Text("Enter your yearly salary post tax")
+            TextField("", value: $takeHomePay, format: .number)
                 .multilineTextAlignment(.center)
             Text("Enter money for 401k")
             TextField("Money for 401k", value: $retirementAccount, format: .number)
                 .multilineTextAlignment(.center)
             Button {
                 CalculatePost401k()
+                
             } label: {
                 ZStack{
                     Circle()
