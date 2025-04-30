e5be6ee2b7bf9ffd1e95ab7b89c8f929810a8ca9
Matei Donisa <mdonisa8105@stu.d214.org>
2025-04-30 13:15:13 -0500

    I did a dark mode in the 401k section

--- RetirementAccount.swift
+++ RetirementAccount.swift
@@ -6,70 +6,78 @@ struct RetirementAccountView: View {
     @State var moneyPost401k: Double =  0
     @State var age: Double = 0
     @State var output: String = ""
+    @State var Background: Bool = false
         var body: some View{
-            Text("Enter your age")
-            TextField("", value: $age, format: .number)
-                .multilineTextAlignment(.center)
-            Button("Find out Maximum Limit for 401k"){
-                if age <= 49 {
-                    output = "The maximum amount you can put in your 401k is $23,500"
+            VStack{
+                Text("Enter your age")
+                TextField("", value: $age, format: .number)
+                    .multilineTextAlignment(.center)
+                Button("Find out Maximum Limit for 401k"){
+                    if age <= 49 {
+                        output = "The maximum amount you can put in your 401k is $23,500"
+                    }
+                    if age == 50 {
+                        output = "The maximum amount you can put in your 401k is $31,000"
+                    }
+                    if age == 51 {
+                        output = "The maximum amount you can put in your 401k is $31,000"
+                    }
+                    if age == 52 {
+                        output = "The maximum amount you can put in your 401k is $31,000"
+                    }
+                    if age == 53 {
+                        output = "The maximum amount you can put in your 401k is $31,000"
+                    }
+                    if age == 54 {
+                        output = "The maximum amount you can put in your 401k is $31,000"
+                    }
+                    if age == 55 {
+                        output = "The maximum amount you can put in your 401k is $31,000"
+                    }
+                    if age == 56 {
+                        output = "The maximum amount you can put in your 401k is $31,000"
+                    }
+                    if age == 57 {
+                        output = "The maximum amount you can put in your 401k is $31,000"
+                    }
+                    if age == 58 {
+                        output = "The maximum amount you can put in your 401k is $31,000"
+                    }
+                    if age == 59 {
+                        output = "The maximum amount you can put in your 401k is $31,000"
+                    }
+                    if age >= 60 {
+                        output = "The maximum amount you can put in your 401k is $34,750"
+                    }
                 }
-                if age == 50 {
-                    output = "The maximum amount you can put in your 401k is $31,000"
+                Text("\(output)")
+                Text("Enter your yearly salary post tax")
+                TextField("", value: $takeHomePay, format: .number)
+                    .multilineTextAlignment(.center)
+                Text("Enter money for 401k")
+                TextField("Money for 401k", value: $retirementAccount, format: .number)
+                    .multilineTextAlignment(.center)
+                Button {
+                    CalculatePost401k()
+                    
+                } label: {
+                    ZStack{
+                        Circle()
+                            .frame(width:90, height:90)
+                            .foregroundStyle(.yellow)
+                        Text("Calculate")
+                            .foregroundStyle(.blue)
+                        
+                    }
                 }
-                if age == 51 {
-                    output = "The maximum amount you can put in your 401k is $31,000"
-                }
-                if age == 52 {
-                    output = "The maximum amount you can put in your 401k is $31,000"
-                }
-                if age == 53 {
-                    output = "The maximum amount you can put in your 401k is $31,000"
-                }
-                if age == 54 {
-                    output = "The maximum amount you can put in your 401k is $31,000"
-                }
-                if age == 55 {
-                    output = "The maximum amount you can put in your 401k is $31,000"
-                }
-                if age == 56 {
-                    output = "The maximum amount you can put in your 401k is $31,000"
-                }
-                if age == 57 {
-                    output = "The maximum amount you can put in your 401k is $31,000"
-                }
-                if age == 58 {
-                    output = "The maximum amount you can put in your 401k is $31,000"
-                }
-                if age == 59 {
-                    output = "The maximum amount you can put in your 401k is $31,000"
-                }
-                if age >= 60 {
-                    output = "The maximum amount you can put in your 401k is $34,750"
-                }
-            }
-            Text("\(output)")
-            Text("Enter your yearly salary post tax")
-            TextField("", value: $takeHomePay, format: .number)
-                .multilineTextAlignment(.center)
-            Text("Enter money for 401k")
-            TextField("Money for 401k", value: $retirementAccount, format: .number)
-                .multilineTextAlignment(.center)
-            Button {
-                CalculatePost401k()
+                Text(String("Money left after 401k: $\(moneyPost401k)"))
                 
-            } label: {
-                ZStack{
-                    Circle()
-                        .frame(width:90, height:90)
-                        .foregroundStyle(.yellow)
-                    Text("Calculate")
-                        .foregroundStyle(.blue)
-                   
-                }
+                Toggle("Dark Mode",isOn: $Background)
+                    .padding(.bottom)
             }
-            Text(String("Money left after 401k: $\(moneyPost401k)"))
-            
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(Background ? .black.opacity(0.9): .white)
+            .foregroundStyle(Background ? .white: .black)
     }
     func CalculatePost401k (){
         moneyPost401k = takeHomePay - retirementAccount
