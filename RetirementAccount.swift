b8966fddd1eb119012c08525070c22ac0745626a
Christopher Sebastian <csebastian8396@stu.d214.org>
2025-05-12 11:03:38 -0700

    I added a Navigation Link to Housing.

--- RetirementAccount.swift
+++ RetirementAccount.swift
@@ -2,7 +2,8 @@ import SwiftUI
 
 struct RetirementAccountView: View {
     @State var retirementAccount: Double = 0
-    @Binding var takeHomePay: Double 
+    @State var housing: Double = 0
+    @Binding var takeHomePay: Double
     @State var moneyPost401k: Double =  0
     @State var age: Double = 0
     @State var amount: Int = 0
@@ -10,65 +11,72 @@ struct RetirementAccountView: View {
     @State var monkey: String = ""
     @State var Background: Bool = false
     var body: some View {
-        VStack{
-            Text("Enter your age")
-                .foregroundStyle(Background ? .white: .black)
-            TextField("", value: $age, format: .number)
-                .multilineTextAlignment(.center)
-                .foregroundStyle(Background ? .white: .black)
-            Button("Find out Maximum Limit for 401k"){
-                if age <= 49 {
-                    amount = 23500
-                    output = ("The maximum amount you can put in your 401k is \(amount.formatted(.currency(code: "USD")))")
-                }
-                if age >= 50 && age <= 59 {
-                    amount = 31000
-                    output = "The maximum amount you can put in your 401k is \(amount.formatted(.currency(code: "USD")))"
-                }
-                if age >= 60 && age <= 63 {
-                    amount = 34750
-                    output = "The maximum amount you can put in your 401k is \(amount.formatted(.currency(code: "USD")))"
+        NavigationStack {
+            
+        
+            VStack{
+                Text("Enter your age")
+                    .foregroundStyle(Background ? .white: .black)
+                TextField("", value: $age, format: .number)
+                    .multilineTextAlignment(.center)
+                    .foregroundStyle(Background ? .white: .black)
+                Button("Find out Maximum Limit for 401k"){
+                    if age <= 49 {
+                        amount = 23500
+                        output = ("The maximum amount you can put in your 401k is \(amount.formatted(.currency(code: "USD")))")
+                    }
+                    if age >= 50 && age <= 59 {
+                        amount = 31000
+                        output = "The maximum amount you can put in your 401k is \(amount.formatted(.currency(code: "USD")))"
+                    }
+                    if age >= 60 && age <= 63 {
+                        amount = 34750
+                        output = "The maximum amount you can put in your 401k is \(amount.formatted(.currency(code: "USD")))"
+                    }
+                    
+                    if age >= 64 {
+                        amount = 31000
+                        output = "The maximum amount you can put in your 401k is \(amount.formatted(.currency(code: "USD")))"
+                    }
                 }
                 
-                if age >= 64 {
-                    amount = 31000
-                    output = "The maximum amount you can put in your 401k is \(amount.formatted(.currency(code: "USD")))"
-                }
-            }
-            
-            Text("\(output)")
-                .foregroundStyle(Background ? .white: .black)
-            Text("Enter your yearly salary post tax")
-                .foregroundStyle(Background ? .white: .black)
-            TextField("", value: $takeHomePay, format: .number)
-                .multilineTextAlignment(.center)
-                .foregroundStyle(Background ? .white: .black)
-            Text("Enter money for 401k")
-                .foregroundStyle(Background ? .white: .black)
-            TextField("Money for 401k", value: $retirementAccount, format: .number)
-                .multilineTextAlignment(.center)
-                .foregroundStyle(Background ? .white: .black)
-            
-            
-            
-            
-            Button {
-                CalculatePost401k()
+                Text("\(output)")
+                    .foregroundStyle(Background ? .white: .black)
+                Text("Enter your yearly salary post tax")
+                    .foregroundStyle(Background ? .white: .black)
+                TextField("", value: $takeHomePay, format: .number)
+                    .multilineTextAlignment(.center)
+                    .foregroundStyle(Background ? .white: .black)
+                Text("Enter money for 401k")
+                    .foregroundStyle(Background ? .white: .black)
+                TextField("Money for 401k", value: $retirementAccount, format: .number)
+                    .multilineTextAlignment(.center)
+                    .foregroundStyle(Background ? .white: .black)
                 
-            } label: {
-                ZStack{
-                    Circle()
-                        .frame(width:90, height:90)
-                        .foregroundStyle(.yellow)
-                    Text("Calculate")
-                        .foregroundStyle(.blue)
+                
+                
+                
+                Button {
+                    CalculatePost401k()
                     
-                    
-                    
+                } label: {
+                    ZStack{
+                        Circle()
+                            .frame(width:90, height:90)
+                            .foregroundStyle(.yellow)
+                        Text("Calculate")
+                            .foregroundStyle(.blue)
+                        
+                        
+                        
+                    }
+                }
+                Text(String("Money left after 401k: $\(moneyPost401k)"))
+                    .foregroundStyle(Background ? .white: .black)
+                NavigationLink("Go to Housing Costs") {
+                    HousingView(moneyPost401k: $housing )
                 }
             }
-            Text(String("Money left after 401k: $\(moneyPost401k)"))
-                .foregroundStyle(Background ? .white: .black)
             
             Toggle("Dark Mode",isOn: $Background)
                 .padding(.bottom)
